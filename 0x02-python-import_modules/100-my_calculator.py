267ac5ad32f5a05cb50198168ba0191b62f3911b
robertokoba7 <okobarobert7@gmail.com>
2022-08-25 17:14:47 +0300

    own calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,10 +2,10 @@
 if __name__ == "__main__":
     import sys
 
-    nargs = len(sys.argv) -1
-    if nargs != 3 
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
+    nargs = len(sys.argv) - 1
+    if nargs != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
     op = sys.argv[2]
      if op != '+' and op != '-' and op != '*' and op != '/':
