1471f7ebfa47289f3bb43b5f2679606e41e82df0
robertokoba7 <okobarobert7@gmail.com>
2022-08-27 14:45:07 +0300

    Retrieve element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx == "-":
+    if idx < 0:
         return (None)
-    elif idx > my_list:
+    length = len(my_list)
+
+    if idx > length - 1:
         return (None)
-    else:
-        print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
+    
+        return(my_list[idx])
