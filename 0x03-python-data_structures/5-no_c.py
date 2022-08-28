e8650841c99eba092ce5940bf8c7cf9ea4c9302c
robertokoba7 <okobarobert7@gmail.com>
2022-08-28 14:16:03 +0300

    remove c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def no_c(my_string):
-     counts = my_string.count('c')
-     new_string = list(my_string)
-     while counts:
-         new_string.remove('c')
-         counts -= 1
-     counts = my_string.count('C')
-     while counts:
-         new_string.remove('C')
-         counts -= 1
-    new_string = ''.join(new_string)
-    return new_string
+     length = len(my_string)
+     j = 0
+     new_string = my_string[:]
+
+     for i in range(length):
+         if (my_string[i] == 'c' or my_string[i] == 'C'):
+             new_string = new_string[:(i - j)] + my_string[(i + 1):]
+             j += 1
+     return (new_string)
