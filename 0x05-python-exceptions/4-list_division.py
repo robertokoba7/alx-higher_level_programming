f91e8c173dd7b011c39db498ef15afcc04934911
robertokoba7 <okobarobert7@gmail.com>
2022-09-12 17:49:22 +0300

    Integers division with debug

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+
+
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    for i in range(0, list_length):
+    for i in range(list_length):
         try:
-            result = my_list_1[i] / my_list_2[i]
+            div = my_list_1[i] / my_list_2[i]
         except TypeError:
             print("wrong tye")
             div = 0
