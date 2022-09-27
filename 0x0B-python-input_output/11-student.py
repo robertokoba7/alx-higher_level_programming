8fbc0c3572d97e3a323092aafba6c709272cf3a0
robertokoba7 <okobarobert7@gmail.com>
2022-09-27 20:09:01 +0300

    Student to disk and reload

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -29,7 +29,7 @@ class Student:
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
-    
+
     def reload_from_json(self, json):
         """Replace all attributes of the Student.
         Args:
