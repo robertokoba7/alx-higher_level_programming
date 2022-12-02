6cedc01e079a87c4f62ad6a03ae6ebf70073772f
robertokoba7 <okobarobert7@gmail.com>
2022-12-02 10:15:29 +0300

    Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""lists all states with a name starting
+with N (upper N) from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(
+            host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor_db = db.cursor()
+    cursor_db.execute("SELECT * FROM states\
+    WHERE name REGEXP BINARY '^N' ORDER BY id ASC")
+    states = cursor_db.fetchall()
+    for state in states:
+        print(state)
+    cursor_db.close()
+    db.close()
