d74db7678b53e7f5ddfc9555aea7f8505a1b3482
robertokoba7 <okobarobert7@gmail.com>
2022-12-03 09:28:46 +0300

    filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,9 +6,14 @@ import MySQLdb
 import sys
 if __name__ == "__main__":
 
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3] port=3306)
+
     cursor_db = db.cursor()
-    cursor_db.execute("SELECT * FROM `states` ORDER BY id")
+    cursor_db.execute("SELECT *\ 
+    FROM states \
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS \
+    LIKE 'N%';")
     states = cursor_db.fetchall()
     for state in states:
         print(state)
