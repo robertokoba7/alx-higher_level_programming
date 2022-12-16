7e77d49a25e0da94e2ef682a2f25682e05242a04
robertokoba7 <okobarobert7@gmail.com>
2022-12-16 19:10:24 +0300

    [Feature]: adding a script that sends a POST with a variable

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
-takes in a URL and an email, sends a POST request to the passed URL with the
-email as a parameter, and displays the body of the response
+Python script that takes in a URL and an email, sends a POST
+request to the passed URL with the email as a parameter, and
+displays the body of the response
 """
-
 from urllib import request, parse
 import sys
+
 
 if __name__ == "__main__":
     values = {'email': sys.argv[2]}
