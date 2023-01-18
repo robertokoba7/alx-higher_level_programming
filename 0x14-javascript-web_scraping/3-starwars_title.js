1590bcf9f026f2be3c4d90685ef4d46ce5aa6320
robertokoba7 <okobarobert7@gmail.com>
2023-01-18 19:17:35 +0300

    [add]:title of star Wars & episode # given int

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+const url = 'https://swapi-api.hbtn.io/api/films/';
+const ep = process.argv[2];
+request(url + ep, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const jsonobj = JSON.parse(body);
+    console.log(jsonobj.title);
+  } else {
+    console.log('error ocurred or chapter not found. Status code: ' + response.statusCode);
+  }
 });
