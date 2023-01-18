0173b8e8dd7c594b0610646f129533d7639d5868
robertokoba7 <okobarobert7@gmail.com>
2023-01-18 18:02:46 +0300

    [add]:writes a string to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (error, content) {
-  console.log();
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', (err, content) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(content);
 });
